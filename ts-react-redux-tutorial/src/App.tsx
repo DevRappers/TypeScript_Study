309e844c5bc94a98a174b1f385319c2fa65b9cf6
DevRappers <devrappers@gmail.com>
2020-03-11 17:35:29 +0900

    typescript react todolist

--- ts-react-redux-tutorial/src/App.tsx
+++ ts-react-redux-tutorial/src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import CounterContainer from "./components/CounterContainer";
+// import CounterContainer from "./components/CounterContainer";
+import TodoApp from "./components/TodoApp.ts";
 
 function App() {
-  return <CounterContainer />;
+  return <TodoApp/>;
 }
 
 export default App;
